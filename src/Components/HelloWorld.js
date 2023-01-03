e0e29e55bfd6bde1907b3d523f96c4e477619515
Kiflan Adli <kiflanadli@gmail.com>
2023-01-03 23:14:15 +0700

    adjustment to react-v16

--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -1,7 +1,7 @@
+import React from "react";
+
 function HelloWorld(props) {
-    return (
-        <h1>Hello {props.name}</h1>
-    )
+  return <h1>Hello {props.name}</h1>;
 }
 
 export default HelloWorld;
