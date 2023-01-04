898d1459541c1cbe165881fbe896ad84afd402bd
Kiflan Adli <kiflanadli@gmail.com>
2023-01-04 15:29:29 +0700

    fix footer layout

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Product from "./Views/Product";
 
 function App() {
   return (
-    <div>
+    <div className="relative pb-10 min-h-screen">
       <Router>
         <Header />
         <div className="p-3">
