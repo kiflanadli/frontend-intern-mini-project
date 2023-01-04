4c4c9991d542dbe80fee8656611b0f69f423660a
Kiflan Adli <kiflanadli@gmail.com>
2023-01-04 14:31:12 +0700

    conditional rendering -- loading & error

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,29 +1,58 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import Loader from "../Components/Loader";
 
 export default function Product() {
   const { id } = useParams();
   const url = `https://63b50ba39f50390584bf6aea.mockapi.io/api/v1/products/${id}`;
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
 
   let content;
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setProduct(response.data);
+    setProduct({
+      loading: true,
+      data: null,
+      error: false,
     });
+    axios
+      .get(url)
+      .then((response) => {
+        setProduct({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch(() => {
+        setProduct({
+          loading: false,
+          data: null,
+          error: true,
+        });
+      });
   }, [url]);
 
-  if (product) {
+  if (product.error) {
+    content = <p>There was an error.</p>;
+  }
+  if (product.loading) {
+    content = <Loader />;
+  }
+  if (product.data) {
     content = (
       <div>
-        <h1 className="text-2xl font-bold mb-3">{product.name}</h1>
+        <h1 className="text-2xl font-bold mb-3">{product.data.name}</h1>
         <div>
-          <img src={product.images[0].imageUrl} alt={product.name} />
+          <img src={product.data.images[0].imageUrl} alt={product.data.name} />
         </div>
-        <div className="font-bold text-xl mb-3">$ {product.price}</div>
-        <div>{product.description}</div>
+        <div className="font-bold text-xl mb-3">$ {product.data.price}</div>
+        <div>{product.data.description}</div>
       </div>
     );
   }
