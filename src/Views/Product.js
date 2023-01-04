95721bce12db0999e5049fe70af18847b99eff02
Kiflan Adli <kiflanadli@gmail.com>
2023-01-04 15:49:44 +0700

    use custom hooks to fetch data

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -1,42 +1,15 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import { useParams } from "react-router-dom";
 import Loader from "../Components/Loader";
+import { useAxiosGet } from "../Hooks/HttpRequests";
 
 export default function Product() {
   const { id } = useParams();
   const url = `https://63b50ba39f50390584bf6aea.mockapi.io/api/v1/products/${id}`;
-  const [product, setProduct] = useState({
-    loading: false,
-    data: null,
-    error: false,
-  });
+
+  let product = useAxiosGet(url);
 
   let content = null;
-
-  useEffect(() => {
-    setProduct({
-      loading: true,
-      data: null,
-      error: false,
-    });
-    axios
-      .get(url)
-      .then((response) => {
-        setProduct({
-          loading: false,
-          data: response.data,
-          error: false,
-        });
-      })
-      .catch(() => {
-        setProduct({
-          loading: false,
-          data: null,
-          error: true,
-        });
-      });
-  }, [url]);
 
   if (product.error) {
     content = <p>There was an error.</p>;
